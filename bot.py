3770f00eadcf3038572a319ce14a7347ca934439
andrewijvs <43344576+andrewijvs@users.noreply.github.com>
2018-09-20 16:22:25 +0300

    'df'

--- bot.py
+++ bot.py
@@ -3,20 +3,21 @@ import text
 from re import match
 
 token = '637861645:AAEYDWviuBn8KpSDp6Un_copMZTeevecDFw'
-<<<<<<< HEAD
 bot = telebot.TeleBot(token)
 markup = telebot.types.ReplyKeyboardMarkup()
 markup.row('Прайс')
 markup.row('Работа', 'Информация')
 markup.row('Оплатил "получить товар"', 'Отмена заказа')
-druglist = {'amf': [('1г', 1200), ('3г', 2700),
-			        ('5г', 4000), ('10г', 6500)],
-            'hashish': [('1г', 700), ('3г', 1800),
-			            ('5г', 2500), ('10г', 4500)],
-            'lsd': [('1шт', 700), ('3шт', 1800),
-			            ('5шт', 2500), ('10шт', 4500)],
-			'mdma': [('1шт', 1000), ('3шт', 2100),
-			            ('5шт', 3000), ('10шт', 5000)]}
+druglist = {'amf': ([('1г', 1200), ('3г', 2700),
+			        ('5г', 4000), ('10г', 6500)], 'Амфетамин'),
+            'hashish': ([('1г', 700), ('3г', 1800),
+			            ('5г', 2500), ('10г', 4500)], 'Амфетамин'),
+            'lsd': ([('1шт', 800), ('3шт', 2000),
+			            ('5шт', 3000), ('10шт', 4500)], 'Амфетамин'),
+			'mdma': ([('1шт', 1000), ('3шт', 2100),
+			            ('5шт', 3000), ('10шт', 5000)], 'Амфетамин'),
+			'chris': ([('0.5г', 1200), ('1г', 2000), ('3г', 4000),
+			            ('5г', 6000), ('10г', 10000)], 'Амфетамин')}
 
 
 @bot.message_handler(content_types=['text'])
@@ -38,63 +39,10 @@ def getMessageText(message):
 		var = match(r'/[a-z]+\d', mText).group(0)[1:]
 		drug = var[:-1]
 		mass = var[-1]
-		#if drug == 'amf':
-			#answer = reserved.format
+		if drug == 'amf':
+			answer = reserved.format
 		answer = text.off
 	bot.send_message(message.chat.id, answer, reply_markup=markup)	
-=======
-
-
-def get_updates_json(request):
-	#params = {'timeout': 100, 'offset': None}
-	#response = requests.get(request + 'getUpdates', data=params)
-	response = requests.get(request + 'getUpdates')
-	return response.json()
-	
-def last_update(data):
-	results = data['result']
-	total_updates = len(results) - 1
-	return results[total_updates]
-
-def get_chat_id(update):
-	chat_id = update['message']['chat']['id']
-	return chat_id
-	
-def send_mess(chat, text):
-	params = {'chat_id': chat, 'text': text}
-	response = requests.post(url + 'sendMessage', data=params)
-	return response
-
-#chat_id = get_chat_id(last_update(get_updates_json(url)))
-
-#send_mess(chat_id, 'Sosi pisos')
-
-
-def main():
-	a = 0
-	bot = telebot.TeleBot(token)
-	markup = telebot.types.ReplyKeyboardMarkup()
-	markup.row('Прайс')
-	markup.row('Работа', 'Информация')
-	markup.row('Оплатил "получить товар"', 'Отмена заказа')
-	bot.send_message(get_chat_id(last_update(get_updates_json(url))), 'd', reply_markup=markup)
-	
-	bot.send_message(get_chat_id(last_update(get_updates_json(url))), 
-			'hello')
-	update_id = last_update(get_updates_json(url))['update_id']
-	while True:
-		'''
-		var = last_update(get_updates_json(url))['update_id'] 
-		if update_id <= var:
-			update_id = var + 1
-			send_mess(get_chat_id(last_update(get_updates_json(
-					url))),	var)
-		'''
-		bot.send_message(get_chat_id(last_update(get_updates_json(url))), 
-			'hello' + str(a))
-		a += 1
-		sleep(3)
->>>>>>> 0338c69760251e554875754cd051c1d646288fef
 	
 if __name__ == '__main__':
 	bot.polling(none_stop=True)
